3830929fe563dde1d10f7986489b55c639df5568
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-03-15 07:50:25 -0300

    Add: First Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { View } from 'react-native';
+
+import 'config/ReactotronConfig';
+import store from 'store';
+
+const App = () => (
+  <Provider store={store}>
+    <View />
+  </Provider>
+);
+
+export default App;
